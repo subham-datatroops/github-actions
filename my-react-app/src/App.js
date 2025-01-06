921a9866757c6140e64f49884e74741843a87900
subham-datatroops <subham.kumar@datatroops.io>
2025-01-06 18:45:26 +0530

    edited App.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hi i'm editing 
+          Edit <code>src/App.js</code> and save to reload. Hi i'm editing something.
         </p>
         <a
           className="App-link"
